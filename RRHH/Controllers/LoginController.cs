1906bc5b22a12d28f5e8bd525e0eaee6e65a411a
Patricio <pa.mikolay@gmail.com>
2017-03-05 15:58:18 -0300

    vista login html y css

--- RRHH/Controllers/LoginController.cs
+++ RRHH/Controllers/LoginController.cs
@@ -9,7 +9,7 @@ namespace RRHH.Controllers
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Index()
+        public ActionResult Login()
         {
             return View();
         }
