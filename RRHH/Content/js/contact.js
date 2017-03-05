8c8fe73b6ddfe9b33bbf4268471d67a2a698de75
Patricio <pa.mikolay@gmail.com>
2017-03-05 17:26:43 -0300

    agrego inputs a REGISTRARSE

---
+++ RRHH/Content/js/contact.js
@@ -0,0 +1,112 @@
+$(document).ready(function () {
+    $(':input[type="submit"]').prop('disabled', true);
+
+    $('#contact_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            first_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                    },
+                        notEmpty: {
+                        message: 'Por favor ingrese su nombre'
+                    }
+                }
+            },
+             last_name: {
+                validators: {
+                     stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Por favor ingrese su apellido'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Por favor ingrese su e-mail'
+                    },
+                    emailAddress: {
+                        message: 'Por favor ingrese una dirección valida de e-mail'
+                    }
+                }
+            },
+            phone: {
+                validators: {
+                    notEmpty: {
+                        message: 'Por favor ingrese su telefono'
+                    },
+                    phone: {
+                        country: 'AR',
+                        message: 'Por favor ingrese un número valido con codigo de area'
+                    }
+                }
+            },
+            address: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Por favor ingrese su dirección'
+                    }
+                }
+            },
+            city: {
+                validators: {
+                     stringLength: {
+                        min: 4,
+                    },
+                    notEmpty: {
+                        message: 'Por favor ingrese su ciudad'
+                    }
+                }
+            },
+            state: {
+                validators: {
+                    notEmpty: {
+                        message: 'Por favor seleccione su provincia'
+                    }
+                }
+            },
+            zip: {
+                validators: {
+                    notEmpty: {
+                        message: 'Por favor ingrese su codigo postal'
+                    },
+                    zipCode: {
+                        country: 'AR',
+                        message: 'Por favor ingrese un codigo postal valido'
+                    }
+                }
+            },
+            }
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+});
+
