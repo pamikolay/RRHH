28e1b5963b1e233bd9b2befc8e19209479b9a4b7
Patricio <pa.mikolay@gmail.com>
2017-04-05 12:18:52 -0300

    se modifico el boton verCV y corrigio el bug del envio del mail al cambiar el estado del Cv

--- RRHH/Models/ApplicantsManager.cs
+++ RRHH/Models/ApplicantsManager.cs
@@ -108,7 +108,7 @@ namespace RRHH.Models
 
         public Applicants ConsultarEstado(int busqueda_id, int user_id)
         {
-            string sqlquery = "SELECT        Applicants.ID, Applicants.Date, Applicants.Postulant, Applicants.Job, Applicants.InterviewStatus, Applicants.ApplicationStatus, Users.FirstName, Users.Email, Users.ID AS UserID, " +
+            string sqlquery = "SELECT        Applicants.ID, Applicants.Date, Applicants.Postulant, Applicants.Job, Applicants.InterviewStatus, Applicants.ApplicationStatus, Users.FirstName, Users.LastName, Users.Email, Users.ID AS UserID, " +
                                 "Companys.Name AS CompanyName, Jobs.Company, Jobs.Name AS JobName, Jobs.Description, Jobs.Status, JobStatuses.Details, JobStatuses.ID AS JobStatusID, " +
                                 "JobApplications.ID AS JobApplicationID, JobApplications.Details AS JobApplicationDetails, dbo.Interviews.Status AS InterviewStatusName, Interviews.ID AS InterviewID " +
                                 "FROM            dbo.Applicants INNER JOIN " +
@@ -134,6 +134,7 @@ namespace RRHH.Models
                 apply.Postulant = new Users();
                 apply.Postulant.ID = (int)reader["UserID"];
                 apply.Postulant.FirstName = (string)reader["FirstName"];
+                apply.Postulant.LastName = (string)reader["LastName"];
                 apply.Postulant.Email= (string)reader["Email"];
                 apply.Job = new Jobs();
                 apply.Job.Name = (string)reader["JobName"];
@@ -143,8 +144,10 @@ namespace RRHH.Models
                 apply.Job.Status = new JobStatuses();
                 apply.Job.Status.Details = (string)reader["Details"];
                 apply.ApplicationStatus = new JobApplications();
+                apply.ApplicationStatus.ID = (int)reader["JobApplicationID"];
                 apply.ApplicationStatus.Details = (string)reader["JobApplicationDetails"];
                 apply.InterviewStatus = new Interviews();
+                apply.InterviewStatus.ID=(int)reader["InterviewID"];
                 apply.InterviewStatus.Status = (string)reader["InterviewStatusName"];
             }
             
@@ -206,8 +209,10 @@ namespace RRHH.Models
                 apply.Job.Status = new JobStatuses();
                 apply.Job.Status.Details = (string)reader["Details"];
                 apply.ApplicationStatus = new JobApplications();
+                apply.ApplicationStatus.ID = (int)reader["JobApplicationID"];
                 apply.ApplicationStatus.Details = (string)reader["JobApplicationDetails"];
                 apply.InterviewStatus = new Interviews();
+                apply.InterviewStatus.ID = (int)reader["InterviewID"];
                 apply.InterviewStatus.Status = (string)reader["InterviewStatusName"];
             }
 
