d06c9f1aef0217a0ccc1179ff90a1101dc06dbcb
Patricio <pa.mikolay@gmail.com>
2017-04-08 15:29:14 -0300

    Fixed css al subir el proyecto a somee

--- RRHH/App_Start/BundleConfig.cs
+++ RRHH/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace RRHH
                       "~/Content/js/bootstrap.min.js", "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css",
+                      "~/Content/css/bootstrap.min.css",
                       "~/Content/css/layout.css"));
         }
     }
